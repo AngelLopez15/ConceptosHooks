5883d9e32ba3b4424b1f63fd016afbccdcd3bff8
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-18 17:33:04 -0500

    Agregue ejemplo de useRef

---
+++ src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+import '../02-useEffect/effect.css'
+
+export const FocusScreen = () => {
+    
+    const inputRef = useRef()
+
+    const handleClick =()=>{
+        // Esta seria la forma tradicional de obtener un elemento
+        // o poniendole un id
+        // document.querySelector('input').select()
+
+        // esta es la forma usando el useRef
+        inputRef.current.select()
+    }
+
+    return (
+        <div>
+            <h1>Focus Screen</h1>
+            <hr />
+            <input
+                // debemos pasar la referencia en el elemento que queremos 
+                // que busca el useRef
+                ref={inputRef}
+                className="form-control"
+                placeholder="Nombre"
+            />
+
+            <button
+                className="btn btn-outline-primary mt-5"
+                onClick={handleClick}
+            >
+                Ficus
+            </button>
+        </div>
+    )
+}
